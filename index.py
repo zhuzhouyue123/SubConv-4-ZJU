0d362453aeeb2430225d5f99ebe802517edc53e3
Geniucker <wolf2003rain@163.com>
2022-11-30 11:55:51 +0800

    fix

--- index.py
+++ index.py
@@ -46,7 +46,7 @@ def test():
     import urllib.request
     url = request.args
     url = url.get("url")
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.56'}
     Request = urllib.request.Request(url)
     r = urllib.request.urlopen(Request).read()
     return r.decode('utf-8')
