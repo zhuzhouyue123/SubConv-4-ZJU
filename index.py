fb193f0ab695b1ec1b59a176aaa460c4c72ece82
Geniucker <wolf2003rain@163.com>
2022-11-30 11:56:44 +0800

    fix

--- index.py
+++ index.py
@@ -49,7 +49,7 @@ def test():
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.56'}
     Request = urllib.request.Request(url)
     r = urllib.request.urlopen(Request).read()
-    return r.decode('utf-8')
+    return r.decode()
 
 
 if __name__ == "__main__":
