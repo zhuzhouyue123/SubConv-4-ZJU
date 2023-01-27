009616f283ddc4f09da9b714cf97cf48c09dbbe3
Geniucker <wolf2003rain@163.com>
2023-01-27 09:28:22 +0800

    remove the testing part

--- index.py
+++ index.py
@@ -2,7 +2,7 @@
 import snippet
 import indexhtml
 import os
-from flask import Flask, request, abort, render_template
+from flask import Flask, request, abort
 from requests import get
 from urllib.parse import urlencode
 from gevent import pywsgi
@@ -38,17 +38,6 @@ def sub():
         + "/api/convert?" + urlencode(urltem)
     result = snippet.pack(url, interval)
     return result, {'Content-Type': 'text/yaml;charset=utf-8'}
-
-
-@app.route("/test")
-def test():
-    import urllib.request
-    url = request.args
-    url = url.get("url")
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.56'}
-    Request = urllib.request.Request(url)
-    r = urllib.request.urlopen(Request).read()
-    return r.decode()
 
 
 if __name__ == "__main__":
