9fcc43c6cb0cf40ad8cdcec717adb5b21eedc585
Geniucker <wolf2003rain@163.com>
2023-03-29 17:45:48 +0800

    revert fix

--- modules/head.py
+++ modules/head.py
@@ -27,8 +27,8 @@ DNS = """dns:
       - +.zju.edu.cn
   fallback:
     - "https://1.1.1.1/dns-query"
-    - 8.8.8.8
-    - 10.10.0.21
+    - "https://8.8.8.8/dns-query"
+    - "tcp://10.10.0.21"
 """
 
 PROXIES_HEAD = """proxies:
