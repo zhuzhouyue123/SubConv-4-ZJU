15b27e4ca46f4f1fb2bd7bbecec1f948a6e58a34
Geniucker <wolf2003rain@163.com>
2023-07-15 16:19:30 +0800

    use https to perform test

--- modules/head.py
+++ modules/head.py
@@ -32,7 +32,7 @@ PROVIDER_BASE0 = """  subscription{}:
       enable: true
       interval: 60
       # lazy: true
-      url: http://www.apple.com/library/test/success.html
+      url: https://www.apple.com/library/test/success.html
 """
 PROVIDER_BASE1 = """  {}:
     type: http
@@ -44,7 +44,7 @@ PROVIDER_BASE1 = """  {}:
       enable: true
       interval: 60
       # lazy: true
-      url: http://www.apple.com/library/test/success.html
+      url: https://www.apple.com/library/test/success.html
 """
 
 
@@ -65,7 +65,7 @@ PROXY_GROUP_PROXY_MANUAL_SELECT = """  - name: 🚀 手动切换
 """
 PROXY_GROUP_PROXY_AUTO_SELECT = """  - name: ♻️ 自动选择
     type: url-test
-    url: http://www.apple.com/library/test/success.html
+    url: https://www.apple.com/library/test/success.html
     interval: 60
     tolerance: 50
     use:
@@ -73,7 +73,7 @@ PROXY_GROUP_PROXY_AUTO_SELECT = """  - name: ♻️ 自动选择
 """
 PROXY_GROUP_PROXY_FALLBACK = """  - name: 🔯 故障转移
     type: fallback
-    url: http://www.apple.com/library/test/success.html
+    url: https://www.apple.com/library/test/success.html
     interval: 60
     tolerance: 50
     use:
@@ -82,7 +82,7 @@ PROXY_GROUP_PROXY_FALLBACK = """  - name: 🔯 故障转移
 PROXY_GROUP_PROXY_ANYCAST = """  - name: {}
     type: load-balance
     strategy: consistent-hashing
-    url: http://www.apple.com/library/test/success.html
+    url: https://www.apple.com/library/test/success.html
     interval: 60
     tolerance: 50
     use:
@@ -115,7 +115,7 @@ PROXY_GROUP_REJECT_FIRST = """  - name: {}
 """
 PROXY_GROUP_REGION_GROUPS = """  - name: {}
     type: url-test
-    url: http://www.apple.com/library/test/success.html
+    url: https://www.apple.com/library/test/success.html
     interval: 60
     tolerance: 50
     use:
